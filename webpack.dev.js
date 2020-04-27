7b7b2ba67ee87f257f45a91d57226a46dd70bc7a
soheee94 <hsh8616@naver.com>
2020-04-27 17:19:49 +0900

    dev server setting

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,21 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common");
 
 const config = {
-  mode: "development"
+  mode: "development",
+  devServer: {
+    // new open tab
+    open: true,
+    // error 가 브라우저 화면에 나타남
+    overlay: true,
+    // 라우팅 예외처리 (SPA 사용하는 경우)
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/subpage$/, to: "subpage.html" },
+        { from: /./, to: "404.html" }
+      ]
+    },
+    port: 3333
+  }
 };
 
 module.exports = merge(common, config);
